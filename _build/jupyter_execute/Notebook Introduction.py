ed67652cb3650683da6aa635638fc18279259f7c
Patrick M. Dudas <dudaspm@gmail.com>
2021-07-15 15:28:13 -0400

    updating the Notebook Introduction

--- _build/jupyter_execute/Notebook Introduction.py
+++ _build/jupyter_execute/Notebook Introduction.py
@@ -3,9 +3,55 @@
 
 # # Notebook Introduction
 
-# I know it tradition to have the refences at the end of books, but when you are standing on the shoulders of giants. You thank them first. 
+# ## How to Use this Notebook 
+
+# This notebook allows you to both follow the text and interact with the code directly. 
+# 
+# At the top page, you will see:
+# 
+# ![icons at the top right corner of the screen](https://raw.githubusercontent.com/dudaspm/JupyterLab-Python/main/images/topscreen1.png)
+# 
+# Select the icon on the left, the rocket ship:
+# 
+# ![rocket ship icon for running the notebook](https://raw.githubusercontent.com/dudaspm/JupyterLab-Python/main/images/topscreen2.png)
+# 
+# Then go down to "Live Code":
+# 
+# ![selecting Live Code](https://raw.githubusercontent.com/dudaspm/JupyterLab-Python/main/images/topscreen4.png)
+# 
+# You should see at the top of the page a loading bar that cycles through mulitple states. 
+# 
+# ![loading bar, first cycle](https://raw.githubusercontent.com/dudaspm/JupyterLab-Python/main/images/topscreen6.png)
+# 
+# ![loading bar, second cycle](https://raw.githubusercontent.com/dudaspm/JupyterLab-Python/main/images/topscreen7.png)
+# 
+# ![loading bar, thrid cycle](https://raw.githubusercontent.com/dudaspm/JupyterLab-Python/main/images/topscreen8.png)
+# 
+# Then, finally:
+# 
+# ![loading bar, final cycle](https://raw.githubusercontent.com/dudaspm/JupyterLab-Python/main/images/topscreen9.png)
+# 
+# Try this for yourself! 
+# 
+# Here is a basic line in Python, after setting to Live Code. You can edit the Python code **directly in the notebook** 😀 😀 😀
+
+# In[1]:
+
+
+2 + 2
+
+
+# ## Export to Binder or Google Colab 
+
+# As you can see you can also export these notebooks to Binder or Google Colab. 
+# 
+# ![Bonder or Google Colab options](https://raw.githubusercontent.com/dudaspm/JupyterLab-Python/main/images/topscreen3.png)
+# 
+# This will take you to their respective websites but you can work with them there, if you would like!
 
 # ## References
+
+# I know it tradition to have the refences at the end of books, but when you are standing on the shoulders of giants. You thank them first. 
 
 # ```{bibliography}
 # ```
